47ab9d487e1f93e1ced93e28e09e7ba6f0279bb5
Ilham  Fadhilah <ilham.fadhilah.ft20@mail.umy.ac.id>
2022-10-31 22:30:12 +0700

    start git

---
+++ src/main/java/wsb/basicthymeleaf/BasicthymeleafApplication.java
@@ -0,0 +1,13 @@
+package wsb.basicthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicthymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicthymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wsb/basicthymeleaf/BasicthymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.basicthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicthymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
